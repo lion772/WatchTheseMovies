e50dd49b9205449a271260a110c5c60a3603974b
William <williamlopes64@gmail.com>
2020-09-21 20:04:43 -0300

    Adding Model and extensions to the build gradle/app

--- app/src/main/java/com/example/watchthesemoviestest/network/MovieApi.kt
+++ app/src/main/java/com/example/watchthesemoviestest/network/MovieApi.kt
@@ -1,6 +1,10 @@
 package com.example.watchthesemoviestest.network
 
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface MovieApi {
 
-
+    @GET("3/movie/550?api_key=b3bb363a5285a5d43a349b74bd0454aa")
+    suspend fun getMoviesApi(): Response<List<>>
 }
